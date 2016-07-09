2cd13d166482765a0979ccfecf903596b8553145
Tarcio <tarcio@gmail.com>
2016-07-09 19:01:15 +1000

    inject Karma logger into plugin for better messaging

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var path = require('path')
 var wrapper = require('@pact-foundation/pact-node')
 
-var runPactMockServer = function (opts) {
-  opts = opts || { port: 1234 }
+var runPactMockServer = function (opts, logger) {
+  var log = logger.create('pact')
+
+  if (!opts) {
+    opts = {}
+  }
+
+  opts.port = opts.port || 1234
   opts.log = opts.log || path.resolve(process.cwd(), 'logs', 'mockserver-integration.log')
   opts.dir = opts.dir || path.resolve(process.cwd(), 'pacts')
   opts.spec = opts.spec || 2
@@ -14,14 +20,17 @@ var runPactMockServer = function (opts) {
 
   var server = wrapper.createServer(opts)
   server.start()
-  console.log('Provider Mock Server running on port: ' + opts.port)
+  server.on('start', function () {
+    log.info('Provider Mock Server running on port: ' + opts.port)
+  })
 
   process.on('SIGINT', function () {
+    log.info('Removing all Provider Mock Servers instantiated.')
     wrapper.removeAllServers()
   })
 }
 
-runPactMockServer.$inject = ['config.pact']
+runPactMockServer.$inject = ['config.pact', 'logger']
 
 module.exports = {
   'framework:pact': ['factory', runPactMockServer]
