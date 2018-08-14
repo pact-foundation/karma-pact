6ac16cd2ed76be54138d75d4c54d66fb5064bf73
Mardzy <paul.mardling@itv.com>
2018-08-14 14:30:08 +0100

    chore(build): tidied index

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 var wrapper = require('@pact-foundation/pact-node');
 var deasync = require('deasync');
-var Promise = require('bluebird');
+
+//check to see if each server has started, then it is pushed into an array
 
 var runPactMockServer = function (pacts, logger) {
 	var log = logger.create('pact');
 	pacts = pacts || [];
-	
+	var count = 0;
+
 	// If pact options is object, wrap in array
 	if (!Array.isArray(pacts)) {
 		pacts = [pacts];
 	}
-	
-	var done = false;
-	
-	var count = 0;
 
-	Promise.all(pacts.map(function (pact) {
-
+	function initializeServer(pact) {
 		var server = wrapper.createServer(pact);
-		return server.start().then(function () {
-			log.info('Pact Mock Server running on port: ' + server.options.port);
-			if (++count === pacts.length) {
-				done = true;
-			}
-		}, function (err) {
-			if (++count === pacts.length) {
-				done = true;
-			}
-			log.error('Error while trying to run karma-pact: ' + err);
-			reject(err);
-			throw err;
-		});
-	})).then(function() {
-		done = true;
-	});
+		server.start()
+			.then(function () {
+				log.info('Pact Mock Server running on port: ' + server.options.port);
+				count+=1;
+			})
+			.catch(function(err) {
+				log.error('Error while trying to run karma-pact: ' + err);
+				throw err;
+			});
+	}
+
+	pacts.map(initializeServer);
 
 	deasync.loopWhile(function(){
-		return !done;
+		return count > 0
 	});
 
 };
